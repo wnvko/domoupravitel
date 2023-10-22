a2784abbcdf76247cd88ec80638d377802ecee2e
Milko Venkov <MVenkov@infragistics.com>
2023-10-22 17:58:09 +0300

    feat: teaming and localization

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from './auth/user.service';
+import { changei18n } from '@infragistics/igniteui-angular';
+import { IgxResourceStringsBG } from 'igniteui-angular-i18n';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'domoupravitel-front-end';
 
   constructor(public userService: UserService) { }
-
-  public settings() {
-
+  ngOnInit(): void {
+    changei18n(IgxResourceStringsBG);
   }
+
+  public settings() {}
 }
