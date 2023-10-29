5f4b3cde47714d727414b66814f836ab56ffa542
Milko Venkov <MVenkov@infragistics.com>
2023-10-29 09:53:44 +0200

    chore:  ng update @angular/cli

--- frontend/src/app/auth/guard.service.ts
+++ frontend/src/app/auth/guard.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@angular/core";
-import { CanActivate, Router } from "@angular/router";
+import { Router } from "@angular/router";
 import { UserService } from "./user.service";
 
 @Injectable({
     providedIn: 'root'
 })
-export class GuardService implements CanActivate {
+export class GuardService  {
     constructor(
         private user: UserService,
         private router: Router
