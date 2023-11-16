413cf617fd23268e5d1a9a983ea195fc61c6415d
Milko Venkov <MVenkov@infragistics.com>
2023-11-16 23:09:28 +0200

    feat: clean up dates

--- frontend/src/app/moderator/person-descriptor.service.ts
+++ frontend/src/app/moderator/person-descriptor.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { PersonDescriptor } from '../models/person-descriptor';
+import { cleanDate } from '../shared/util';
 
 @Injectable({
   providedIn: 'root'
@@ -17,21 +18,25 @@ export class PersonDescriptorService {
   private serverUrl = environment.API_URL;
 
   constructor(
-      private _http: HttpClient) { }
+    private _http: HttpClient) { }
 
   public all(): Observable<PersonDescriptor[]> {
-      return this.http.get<PersonDescriptor[]>(`${this.serverUrl}/personDescriptors/all`);
+    return this.http.get<PersonDescriptor[]>(`${this.serverUrl}/personDescriptors/all`);
   }
 
   public create(descriptor: PersonDescriptor): Observable<PersonDescriptor> {
-      return this.http.post<PersonDescriptor>(`${this.serverUrl}/personDescriptors/create`, descriptor);
+    descriptor.registeredOn = cleanDate(descriptor.registeredOn!);
+    descriptor.unRegisteredOn = cleanDate(descriptor.unRegisteredOn!);
+    return this.http.post<PersonDescriptor>(`${this.serverUrl}/personDescriptors/create`, descriptor);
   }
 
   public update(descriptor: PersonDescriptor): Observable<PersonDescriptor> {
-      return this.http.put<PersonDescriptor>(`${this.serverUrl}/personDescriptors/update`, descriptor);
+    descriptor.registeredOn = cleanDate(descriptor.registeredOn!);
+    descriptor.unRegisteredOn = cleanDate(descriptor.unRegisteredOn!);
+    return this.http.put<PersonDescriptor>(`${this.serverUrl}/personDescriptors/update`, descriptor);
   }
 
   public delete(descriptor: PersonDescriptor): Observable<PersonDescriptor> {
-      return this.http.delete<PersonDescriptor>(`${this.serverUrl}/personDescriptors/delete`, { body: descriptor });
+    return this.http.delete<PersonDescriptor>(`${this.serverUrl}/personDescriptors/delete`, { body: descriptor });
   }
 }
--- frontend/src/app/shared/util.ts
+++ frontend/src/app/shared/util.ts
@@ -1,3 +1,6 @@
     export function cleanDate (date: Date): Date {
+        if (!date) {
+            return date;
+        }
         return new Date(date.getTime() - date.getTimezoneOffset() * 60000);
     }
