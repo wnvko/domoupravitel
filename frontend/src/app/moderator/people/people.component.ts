728aa2ebf7381afbe3c292bb47f551f02a590db4
Milko Venkov <MVenkov@infragistics.com>
2024-01-27 20:56:35 +0200

    feat: make properties column sortable

--- frontend/src/app/moderator/people/people.component.ts
+++ frontend/src/app/moderator/people/people.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { CellType, IGridEditDoneEventArgs, IRowDataEventArgs, ISortingOptions, IgxDialogComponent, IgxGridComponent } from '@infragistics/igniteui-angular';
+import { CellType, GridType, IGridEditDoneEventArgs, IGridSortingStrategy, IRowDataEventArgs, ISortingExpression, ISortingOptions, IgxDialogComponent, IgxGridComponent, IgxSorting } from '@infragistics/igniteui-angular';
 import { Observable, Subject, first, takeUntil } from 'rxjs';
 import { Person } from 'src/app/models/person';
+import { PersonDescriptor } from 'src/app/models/person-descriptor';
 import { DeleteComponent } from 'src/app/shared/delete/delete.component';
 import { PeopleService } from '../people.service';
 
@@ -20,9 +21,8 @@ export class PeopleComponent implements OnInit, OnDestroy {
   private dialog!: IgxDialogComponent;
 
   public people!: Observable<Person[]>;
-  public sortingOptions: ISortingOptions = {
-    mode: 'single'
-  }
+  public sortingOptions: ISortingOptions = { mode: 'single' };
+  public sortStrategy: IGridSortingStrategy = DescriptorSortingStrategy.instance();
 
   constructor(
     private peopleService: PeopleService
@@ -68,7 +68,41 @@ export class PeopleComponent implements OnInit, OnDestroy {
   }
 
   public renderProperties = (cell: CellType): string => {
-    var person: Person = cell.row.data;
-    return person.descriptors?.map(d => d.property.number).join(', ') ?? '';
+    var descriptors: PersonDescriptor[] = cell.value;
+    return descriptors?.map(d => d.property.number).join(', ') ?? '';
+  }
+}
+
+class DescriptorSortingStrategy implements IGridSortingStrategy {
+  private static _instance: DescriptorSortingStrategy;
+
+  private constructor() { }
+
+  public static instance(): IGridSortingStrategy {
+      return this._instance || (this._instance = new DescriptorSortingStrategy());
+  }
+
+  sort(data: any[], expressions: ISortingExpression<any>[], grid?: GridType | undefined): any[] {
+    if (expressions.length === 1 && expressions[0].fieldName === 'descriptors') {
+      const direction = expressions[0].dir;
+      switch (direction) {
+        case 0:
+          return data;
+        case 1:
+          return data.sort((a: Person, b: Person) => {
+            if (a.descriptors[0]?.property?.number < b.descriptors[0]?.property?.number ) return -1;
+            if (a.descriptors[0]?.property?.number > b.descriptors[0]?.property?.number ) return 1;
+            return 0;
+          });
+        case 2:
+          return data.sort((a: Person, b: Person) => {
+            if (a.descriptors[0]?.property?.number > b.descriptors[0]?.property?.number ) return -1;
+            if (a.descriptors[0]?.property?.number < b.descriptors[0]?.property?.number ) return 1;
+            return 0;
+          });
+      }
+    }
+    const defaultSorting = new IgxSorting();
+    return defaultSorting.sort(data, expressions, grid);
   }
 }
